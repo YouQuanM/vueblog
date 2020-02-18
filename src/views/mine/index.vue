96af36665adb87c67ed492c60510ff4d6c6ddcac
yinhang <614480755@qq.com>
2020-02-18 12:18:04 +0800

    add mine page

--- src/views/mine/index.vue
+++ src/views/mine/index.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="main-page">
     <div class="user-info-area">
-      <div class="user-name">
-        <span>{{userInfo.name}}</span>
+      <div class="user-info-left">
+        <div class="user-name">
+          <span>{{userInfo.name}}</span>
+        </div>
+        <div class="user-introduction">
+          <span>{{userInfo.introduction || '自我介绍'}}</span>
+        </div>
       </div>
       <div class="user-avatar">
-        <el-avatar size="medium" :src="userInfo.avatar || circleUrl"></el-avatar>
-      </div>
-      <div class="user-introduction">
-        <span>{{userInfo.introduction || '自我介绍'}}</span>
+        <el-avatar :src="userInfo.avatar || circleUrl"></el-avatar>
       </div>
     </div>
     <div class="user-logs-area">
-
+      <template v-for="(item, index) in userLogs">
+        <user-log :key="index" :logItem="item"></user-log>
+      </template>
     </div>
   </div>
 </template>
@@ -20,7 +24,9 @@
 <script>
 import { getUserInfo } from '@/common/cookie'
 import * as userApis from '@/apis/user'
+import UserLog from './logs'
 export default {
+  components: {UserLog},
   data () {
     return {
       userInfo: {
@@ -45,6 +51,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.main-page {
+  padding: 12px;
+  .user-info-area {
+    display: flex;
+    justify-content: space-between;
+    .user-name {
+      span {
+        font-size: 30px;
+      }
+    }
+    .user-introduction {
+      margin: 10px 0 0 0;
+    }
+    .user-avatar {
+      .el-avatar {
+        width: 80px;
+        height: 80px;
+      }
+    }
+  }
+}
 </style>
---
+++ src/views/mine/logs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="userlogs-component">
+    <div class="operator">
+      <span>{{ isYou ? '你' : logItem.operator.name }}</span>
+    </div>
+    <div class="operation">
+      <span>{{ logItem.operationLabel }}了</span>
+    </div>
+    <div class="user">
+      <span>{{ isYou ? '你' : logItem.user.name }}的文章:</span>
+    </div>
+    <div class="article">
+      <span>{{ logItem.article.title }}</span>
+    </div>
+    <div class="comment" v-if="logItem.comment.content">
+      <span>:{{ logItem.comment.content }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getUserInfo } from '@/common/cookie'
+export default {
+  props: {
+    logItem: {
+      type: Object
+    }
+  },
+  methods: {
+    isYou(id) {
+      return id === JSON.parse(getUserInfo()).id
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.userlogs-component {
+  display: flex;
+  margin: 10px 0 0 0;
+  padding: 5px;
+  border: 1px solid #dcdfe6;
+}
+</style>
