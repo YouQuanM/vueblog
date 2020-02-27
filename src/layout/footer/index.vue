82ab34d38f26f30e98f85285be4102e99843acdb
yinhang <yhyouquan320@gmail.com>
2020-02-27 12:20:14 +0800

    添加版权

---
+++ src/layout/footer/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>©2017-2020 liangzhicompany.com 版权所有 ICP证: </span>
+    <a href="http://www.beian.miit.gov.cn">京ICP备17072928号-1</a></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -14,6 +14,9 @@
       <section class="layout-sider">
         <layout-sider></layout-sider>
       </section>
+      <div class="layout-footer" :style="footerWidth">
+        <layout-footer></layout-footer>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +25,7 @@
 import LayoutHeader from './header/index'
 import LayoutContent from './content/index'
 import LayoutSider from './sider/index'
+import LayoutFooter from './footer/index'
 export default {
   data() {
     return {
@@ -33,11 +37,15 @@ export default {
   components: {
     LayoutHeader,
     LayoutContent,
-    LayoutSider
+    LayoutSider,
+    LayoutFooter
   },
   mounted() {
     this.styleObj = {
       width: (this.$refs.layout.offsetWidth - 300) + 'px'
+    }
+    this.footerWidth = {
+      width: (this.$refs.layout.offsetWidth) + 'px'
     }
   }
 }
@@ -59,6 +67,14 @@ export default {
     .layout-sider {
       width: 300px;
     }
+    .layout-footer {
+      width: 100%;
+      padding: 10px 0;
+      position: fixed;
+      bottom: 0;
+      border-top: 1px solid #dcdfe6;
+    }
   }
+  
 }
 </style>
